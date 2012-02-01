9ae1b3278052a063b3e1f2c138c6669ba44c4334
Alejandro Cabrera <cpp.cabrera@gmail.com>
2012-01-31 23:45:18 -0500

    Added example right_bracket_parse.

---
+++ c++11/right_bracket_parse.cpp
@@ -0,0 +1,33 @@
+/**
+ *   C++ Examples - a collection of C++ examples intended to serve as a tutorial
+ *   Copyright (C) 2011 Alejandro Cabrera
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ **/
+#include <cstdio>
+
+template <class T>
+struct X {
+};
+
+int main()
+{
+	// can your compiler handle this parse?
+	X<X<int>> *x{nullptr};
+
+	// output for the sake of silencing compiler warnings
+	printf("%p\n", static_cast<void *>(x));
+
+	return 0;
+}
