e724cab8bc217227b539474573dcc6cca9f4213e
Alejandro Cabrera <cpp.cabrera@gmail.com>
2012-01-31 19:33:52 -0500

    Added constexpr example.
    - config.hpp created

---
+++ c++11/config.hpp
@@ -0,0 +1,9 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#define NUM_CPUS 4
+#define CPU_SPEED_GHZ 2
+#define MEM_SIZE_KBYTES (1 << 21)
+#define STACK_SIZE_BYTES 4096
+
+#endif
--- c++11/constexpr.cpp
+++ c++11/constexpr.cpp
@@ -1,20 +1,46 @@
-/** @file constexpr.cpp
- *
- **/
-/** @file constexpr.cpp
- *
- **/
-#include <cstdint>
+#include <iostream>
+#include <algorithm>
+#include "config.hpp"
+using namespace std;
 
-constexpr uintmax_t
-returnFive()
+constexpr size_t
+imin(const size_t lhs, const size_t rhs)
 {
-   return 5;
+	return (lhs < rhs) ? lhs : rhs;
 }
 
+constexpr size_t
+memoryPartition()
+{
+	return MEM_SIZE_KBYTES / (1 << 16);
+}
+
+constexpr size_t
+cpuFactor()
+{
+	return NUM_CPUS * CPU_SPEED_GHZ;
+}
+
+constexpr size_t
+bufferSize()
+{
+	return imin(STACK_SIZE_BYTES/2, 
+							cpuFactor() * memoryPartition());
+}
+
+// Someone else is welcome to provide the macro
+// equivalent of this code! Also, a template meta-program
+// would also work here.
 int main()
 {
-  uintmax_t arr[5 + returnFive()];
+	// using compile-time function to initialize buffer size
+	// using regular functions would generate a compiler
+	// error
+  uint8_t arr[bufferSize()];
+
+	cout << "arr[" << bufferSize() -1 << "] = " 
+			 << static_cast<size_t>(arr[bufferSize() - 1])
+			 << endl;
 
   return 0;
 }
