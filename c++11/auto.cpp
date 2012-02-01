699a2595dbe218fb0f3ba97eca8e0d2d2a20640e
Alejandro Cabrera <cpp.cabrera@gmail.com>
2012-01-31 19:05:59 -0500

    Updated auto.cpp example.

--- c++11/auto.cpp
+++ c++11/auto.cpp
@@ -2,11 +2,44 @@
 #include <iostream>
 using namespace std;
 
-int main() {  
-  vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+// compile-time factorial
+template <size_t N>
+struct Factorial {
+	static const size_t val = N * Factorial<N-1>::val;
+};
 
-  for(auto iter = begin(v), iend = end(v); iter != iend; ++iter)
-    cout << *iter << endl;
+template <>
+struct Factorial<0> {
+	static const size_t val = 1;
+};
+
+int main() {  
+	// vector initialization using initializer lists
+  vector<int> v = {Factorial<5>::val, 
+									 Factorial<7>::val, 
+									 Factorial<8>::val, 
+									 Factorial<3>::val, 
+									 Factorial<4>::val};
+
+	// old-style iterator use
+	cout << "[";
+	for (vector<int>::const_iterator iter = v.begin(),
+				 iend = v.end(); iter != iend; ++iter)
+    cout << *iter << " ";
+	cout << "]\n";
+		
+	// simplified new-style using type inference and
+	// free functions begin() and end()
+	cout << "[";
+  for (auto iter = begin(v), iend = end(v); iter != iend; ++iter)
+    cout << *iter << " ";
+	cout << "]\n";
+
+	// using range-for with auto
+	cout << "[";
+	for (const auto elem : v)
+		cout << elem << " ";
+	cout << "]\n";
 
   return 0;
 }  
