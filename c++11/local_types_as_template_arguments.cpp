46a0673a88cbba44ae68a51ba8018c840b240d16
Alejandro Cabrera <cpp.cabrera@gmail.com>
2012-02-01 15:52:34 -0500

    Added local_types example.

---
+++ c++11/local_types_as_template_arguments.cpp
@@ -0,0 +1,46 @@
+/**
+ *   C++ Examples - a collection of C++ examples intended to serve as a tutorial
+ *   Copyright (C) 2011 Alejandro Cabrera
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ **/
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+template <class X>
+void f(vector<X>& x)
+{
+	struct Less {
+		bool operator() (const X& lhs, const X& rhs) {
+			return lhs < rhs;
+		}
+	};
+
+	sort(x.begin(), x.end(), Less());
+}
+
+int main()
+{
+	vector<int> x = {4,1,3,7,3,2};
+
+	f(x);
+
+	for (auto i : x) 
+		cout << i << ",";
+	cout << endl;
+	
+	return 0;
+}
